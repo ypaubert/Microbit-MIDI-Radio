662fb8c186fde47babd18ea3b1d5df5206f80cd9
ypaubert <114052247+ypaubert@users.noreply.github.com>
2022-09-21 08:30:46 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,19 +2,7 @@ function Arrêter_la_note_par_radio (canalmidi: number, notemidi: number, groupe
     radio.setGroup(grouperadio)
     radio.sendNumber(1000 * canalmidi + notemidi)
 }
-input.onButtonPressed(Button.A, function () {
-    Arrêter_la_note_par_radio(1, 36, 1)
-    Jouer_la_note_par_radio(1, 1, 1)
-})
 function Jouer_la_note_par_radio (canalmidi: number, notemidi: number, grouperadio: number) {
     radio.setGroup(grouperadio)
     radio.sendNumber(1000 * canalmidi + notemidi)
 }
-input.onGesture(Gesture.ThreeG, function () {
-    Arrêter_la_note_par_radio(1, 36, 1)
-})
-basic.forever(function () {
-    if (input.lightLevel() == 0) {
-        Arrêter_la_note_par_radio(1, 36, 1)
-    }
-})
